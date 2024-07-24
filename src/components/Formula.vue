421e11f9677b90366d840540058e14dccc71ea87
KYLIN-GLOBAL <kylin.dong.us@outlook.com>
2024-07-24 10:36:00 +0800

    feat: add two number and check

---
+++ src/components/Formula.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-html="currentState.mathML"></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const currentState = ref({
+    mathML: '<math><mrow><mn>1</mn><mo>+</mo><mn>2</mn></mrow></math>',
+    answer: null as null | number,
+})
+
+const generateRandomFormula = () => {
+    const a = Math.floor(Math.random() * 10);
+    const b = Math.floor(Math.random() * 10);
+    const answer = a + b;
+    const mathML = `<math><mrow><mn>${a}</mn><mo>+</mo><mn>${b}</mn></mrow></math>`;
+    currentState.value.answer = answer;
+    currentState.value.mathML = mathML;
+}
+
+generateRandomFormula();
+
+defineExpose({generateRandomFormula, currentState})
+
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,43 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Formula from './Formula.vue'
 
-defineProps<{ msg: string }>()
+const userInput = ref('');
+const resultText = ref('Press "check" to submit your answer.')
 
-const count = ref(0)
+const formulaComponent = ref(null as any);
+
+const generateNewFormula = () => {
+  if (formulaComponent.value) {
+    formulaComponent.value.generateRandomFormula();
+    userInput.value = '';
+    resultText.value = 'Press "check" to submit your answer.';
+  }
+}
+
+const checkAnswer = () => {
+  if (userInput != null) {
+    if (userInput.value == formulaComponent.value.currentState.answer) {
+      resultText.value = "Right!"
+    } else {
+      resultText.value = "Try it Again!"
+    }
+  }
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <Formula ref="formulaComponent"/>
+    <label for="answer">Your Answer: </label>
+    <input name="answer" v-model.number="userInput"></input>
+    <p>{{ resultText }}</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <button @click="checkAnswer">check</button>
+  <button @click="generateNewFormula">new question</button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
